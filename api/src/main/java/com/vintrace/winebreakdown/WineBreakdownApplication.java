5686703be817bcf5bbc2c14ab28924199aa14b16
dvdkruk <damiaan.van.der.kruk@gmail.com>
2020-12-13 15:31:45 +1100

    Initial Spring Boot setup

---
+++ api/src/main/java/com/vintrace/winebreakdown/WineBreakdownApplication.java
@@ -0,0 +1,13 @@
+package com.vintrace.winebreakdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineBreakdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineBreakdownApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/vintrace/winebreakdown/WineBreakdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vintrace.winebreakdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineBreakdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
