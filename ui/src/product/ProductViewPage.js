270bfe7c3af85f1599aeb18b5184f40df170ab94
dvdkruk <damiaan.van.der.kruk@gmail.com>
2020-12-15 22:06:02 +1100

    product page with back button

--- ui/src/product/ProductViewPage.js
+++ ui/src/product/ProductViewPage.js
@@ -1,5 +1,26 @@
+import './ProductViewPage.css'
+import Icon from './Icon.png'
+import { useParams, useHistory } from "react-router-dom";
+
 export default function ProductViewPage() {
+    const { lotCode } = useParams();
+    const history = useHistory();
+
     return (
-      <div>hi</div>
+      <div className="ProductViewPage-frame">
+        <div className="ProductViewHeader-frame">
+            <div className="left">
+                <div onClick={history.goBack}><i class="material-icons">arrow_back</i></div>
+                <div>
+                    <div className="left"><img src={Icon} className="ProductViewHeader-icon" alt="Product icon"/></div>
+                    <h2>{lotCode}</h2>
+                </div>
+                <div>description</div>
+            </div>
+            <div className="right">
+                edit
+            </div>
+        </div>
+      </div>
     );
   }
