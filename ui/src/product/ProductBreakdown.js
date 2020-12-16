220bb584ed17aa2f4ab8b3c8e471bc0980d7915b
dvdkruk <damiaan.van.der.kruk@gmail.com>
2020-12-16 21:34:55 +1100

    Breakdown selector + shwoing break down elements

--- ui/src/product/ProductBreakdown.js
+++ ui/src/product/ProductBreakdown.js
@@ -8,42 +8,38 @@ export default function ProductBreakdown({ lotCode }) {
         { key: "region", displayName: "Region" },
         { key: "year-variety", displayName: "Year & Variety" },
     ]
-    const [breakdownType, setbreakdownType] = useState("year");
+    const [breakdownType, setbreakdownType] = useState(breakdownOptions[0]);
     const [breakdown, setbreakdown] = useState([]);
 
-
-
     useEffect(() => {
-        fetchBreakdown(lotCode, breakdownType)
-            .then(breakdown => setbreakdown(breakdown.breakdown))
-    }, [])
+        fetchBreakdown(lotCode, breakdownType.key)
+            .then(({ breakdown }) => setbreakdown(breakdown))
+    }, [breakdownType.key, lotCode])
 
-    console.log(breakdown)
-
+    const breakdownElemtents = breakdown.map(e => <tr key={e.key}><td>{e.key}</td><td>{e.percentage}</td></tr>);
     return (
         <div>
             <BreakdownSelector options={breakdownOptions} onSelection={setbreakdownType} />
             <table>
                 <thead>
                     <tr>
-                        <th>Year</th>
+                        <th>{breakdownType.displayName}</th>
                         <th>Percentage</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {breakdown.map(e => {
-                        return <tr key={e.key}><td>{e.key}</td><td>{e.percentage}</td></tr>
-                    })}
+                    {breakdownElemtents}
                 </tbody>
             </table>
         </div>
     );
 }
+
 function BreakdownSelector({ options, onSelection }) {
-    return <div>
-        {options.map(option => {
-            return <div key={option.key} onClick={() => onSelection(option.key)}>{option.displayName}</div>;
-        })}
-    </div>;
+    return (
+        <div>
+            {options.map(option => <div key={option.key} onClick={() => onSelection(option)}>{option.displayName}</div>)}
+        </div>
+    );
 }
 
