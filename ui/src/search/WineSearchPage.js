77d2c4989515cd3fd237780c3e09bee5125dc56c
dvdkruk <damiaan.van.der.kruk@gmail.com>
2020-12-17 08:58:38 +1100

    Formatted WineSearchPage

--- ui/src/search/WineSearchPage.js
+++ ui/src/search/WineSearchPage.js
@@ -4,16 +4,15 @@ import SearchBar from './components/SearchBar';
 import './search.css'
 
 export default function WineSearchPage() {
-  return (
-    <div className="WineSearchPage">
-      <div className="WineSearchHeader">
-        <header className="HeaderTitle">Wine search</header> 
-        <div className="HeaderIconContainer">
-          <img src={icon} className="HeaderIcon" alt="icon"/>
+    return (
+        <div className="WineSearchPage">
+            <div className="WineSearchHeader">
+                <header className="HeaderTitle">Wine search</header>
+                <div className="HeaderIconContainer">
+                    <img src={icon} className="HeaderIcon" alt="icon" />
+                </div>
+            </div>
+            <SearchBar />
         </div>
-      </div>
-      <SearchBar/>
-    </div>
-  )
+    )
 }
- 
