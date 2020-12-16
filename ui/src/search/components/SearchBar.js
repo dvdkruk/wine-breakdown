c2c92268649617931f483b5ef16e6aca66980b03
dvdkruk <damiaan.van.der.kruk@gmail.com>
2020-12-17 09:04:23 +1100

    Clean up css properties in searchbar

--- ui/src/search/components/SearchBar.js
+++ ui/src/search/components/SearchBar.js
@@ -4,50 +4,47 @@ import findWines from '../findWines';
 import { useHistory } from "react-router-dom";
 
 export default function SearchBar() {
-  const [results, setResults] = useState([]);
+    const [results, setResults] = useState([]);
 
-  async function autoComplete({ target }) {
-    const query = target.value;
-    setResults(query ? await findWines(query) : []);
-  }
+    async function autoComplete({ target }) {
+        const query = target.value;
+        setResults(query ? await findWines(query) : []);
+    }
 
-  return (
-    <div className="SearchBar-frame">
-      <div className="SearchInput-frame">
-        <div className="SearchInput-left">
-          <img src={searchIcon} className="Search-icon left" alt="icon" />
-          <input type="text" onChange={autoComplete} className="SearchInput-input" placeholder="Search by lot code and description......" />
+    return (
+        <div className="SearchBar">
+            <div className="SearchBarInputContainer">
+                <img src={searchIcon} className="SearchIcon" alt="icon" />
+                <input type="text" onChange={autoComplete} className="SearchInput" placeholder="Search by lot code and description......" />
+            </div>
+            <SearchResults results={results} />
         </div>
-        
-      </div>
-        <SearchResults results={results} />
-    </div>
-  );
+    );
 }
 
-function SearchResult({result: wine}) {
-  const history = useHistory();
+function SearchResult({ result: wine }) {
+    const history = useHistory();
 
-  function click() {
-    history.push(`/product/${wine.lotCode}`)
-  }
+    function click() {
+        history.push(`/product/${wine.lotCode}`)
+    }
 
-  return (
-    <div className="SearchResult SearchResult-divider" onClick={click}>
-      <div className="left">
-        <div className="SearchResult-primary">{wine.lotCode}</div>
-        <div className="SearchResult-secondary">{wine.description}</div>
-      </div>
-      <div className="right">
-        <div className="SearchResult-secondary">{wine.volume}</div>
-        <div className="SearchResult-secondary">{wine.tankCode}</div>
-      </div>
-    </div>);
+    return (
+        <div className="SearchResult" onClick={click}>
+            <div className="left">
+                <div className="primary">{wine.lotCode}</div>
+                <div className="secondary">{wine.description}</div>
+            </div>
+            <div className="right">
+                <div className="secondary">{wine.volume}</div>
+                <div className="secondary">{wine.tankCode}</div>
+            </div>
+        </div>);
 }
 
 function SearchResults({ results }) {
-  return <div>
-    {results.map((wine) => <SearchResult key={wine.lotCode} result={wine} />)}
-  </div>;
+    return <div>
+        {results.map((wine) => <SearchResult key={wine.lotCode} result={wine} />)}
+    </div>;
 }
 
